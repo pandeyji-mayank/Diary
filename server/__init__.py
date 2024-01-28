6a92e1bc64154f69d728327c522a53a739f63280
Replit user <>
2024-01-28 17:54:34 +0000

    bugs fixed

--- server/__init__.py
+++ server/__init__.py
@@ -4,23 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
- 
-app = Flask (__name__)
-app.config['SECRET_KEY'] = 'acd98960cc7d85c17eca36fe4a4636e9'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
-db = SQLAlchemy(app) 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SERIAL_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view='login'
-login_manager.login_message_category='info'
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 # app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'mankudevserver@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ihjl zxhx kbfp wyxl'
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
-
-
 
 from server import routes
